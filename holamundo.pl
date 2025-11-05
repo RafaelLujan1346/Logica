05bfcb6f5f70bef52b0e710f9e78f04c02be2eca
RafaelLujan1346 <rafael02lujan@gmail.com>
2025-11-05 14:21:47 -0600

    hola mundo prolog

---
+++ holamundo.pl
@@ -0,0 +1,15 @@
+valioso(oro).
+valioso(plata).
+le_gusta(juan,maria).
+tiene(juan,libro).
+tiene(maria,libro).
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(patricia,jaime).
+
+% progenitor(X,ana),progenitor(Y,X),progenitor(Y,isabel)
+%Es ana tia de jaime? progenitor(X,ana),progenitor(X,Y),progenitor(Y,Z).
+%Es ana tia de isabel? progenitor(X,isabel),progenitor(Y,X),progenitor(Y,ana).
